ca789ce623d2b77438a54402310ff054481f4568
Thomas Brosch <thomas.brosch@stud.tu-ilmenau.de>
2014-04-15 23:49:14 +0200

    AlarmReceiver hinzu

---
+++ src/com/example/autodisconnect/DisconnectAlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.example.autodisconnect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DisconnectAlarmReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
